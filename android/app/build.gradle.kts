4b0ca593213fa287f457fd36f9ddc77f70269880
Crepin <aziamadjicrepin@gmail.com>
2026-09-10 10:02:21 +0000

    fix(ci): appcompat 1.7.0 force (corrige Invalid <color> #6680cbc4) + retire build debug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,6 +10,12 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    configurations.all {
+        resolutionStrategy {
+            force("androidx.appcompat:appcompat:1.7.0")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
