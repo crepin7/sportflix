40b18a493c81a3507f6985b02765190249a73826
Crepin <aziamadjicrepin@gmail.com>
2026-09-26 07:07:40 +0000

    fix(test): titre RichText comparé en texte complet

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,9 +10,8 @@ void main() {
 
   testWidgets('Home screen displays title', (WidgetTester tester) async {
     await tester.pumpWidget(const SportflixApp());
-    // Titre en deux parties : 'Sport' blanc + 'flix' vert thème.
-    expect(find.text('Sport', findRichText: true), findsOneWidget);
-    expect(find.text('flix', findRichText: true), findsOneWidget);
+    // Titre en RichText : 'Sport' blanc + 'flix' vert thème.
+    expect(find.text('Sportflix', findRichText: true), findsOneWidget);
   });
 
   testWidgets('Search field is present', (WidgetTester tester) async {
